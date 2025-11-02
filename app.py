7607976d7ed94e46ffef76aa4842db3dc68aa92e
Gcodsa <gcodfo@gmail.com>
2025-11-02 07:25:49 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -468,6 +468,9 @@ def autocomplete():
     return jsonify(results)
 
 
+import os
+# ...
 if __name__ == "__main__":
-    # للتجربة محليًا
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
